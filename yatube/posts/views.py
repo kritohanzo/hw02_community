4d5acf75a71dcfcf66df60a665a90b798a7807c9
kritohanzo <kritohanzo0@gmail.com>
2022-12-04 18:47:21 +0500

    PEP8 debugging

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from django.template import loader
 from .models import Post, Group
+# from django.http import HttpResponse
+# from django.template import loader
 
 # Create your views here.
 
