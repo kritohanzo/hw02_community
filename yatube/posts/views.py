2f1deaf82398c292970c340875a53951d7f0ccdc
Вагиф Алиев <kritohanzo0@gmail.com>
2022-12-25 18:51:23 +0500

    25_12_2022_17_00_MSK

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,21 +1,28 @@
 from django.shortcuts import get_object_or_404, render
-
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from .models import Group, Post
 
 
 POSTS_PER_PAGE = 10
 
-
+@login_required
 def index(request):
     template = "posts/index.html"
-    posts = Post.objects.all()[:POSTS_PER_PAGE]
-    context = {"posts": posts}
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {"page_obj": page_obj}
     return render(request, template, context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = group.posts.all()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     template = "posts/group_list.html"
-    context = {"group": group, "posts": posts}
+    context = {"group": group, "page_obj": page_obj}
     return render(request, template, context)
--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LogoutView, LoginView, PasswordResetView, PasswordResetDoneView, PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth.views import LogoutView, LoginView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView, PasswordChangeView, PasswordChangeDoneView
 from . import views
 from django.urls import path
 
@@ -10,6 +10,8 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('password_reset/', PasswordResetView.as_view(template_name='users/password_reset_form.html'), name='password_reset_form'),
     path('password_reset/done', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('reset/(?P<uidb64>[^/]+)/(?P<token>[^/]+)/$', PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
     path('password_change/', PasswordChangeView.as_view(template_name='users/password_change_form.html'), name='password_change_form'),
     path('password_change/done/', PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
 ]
